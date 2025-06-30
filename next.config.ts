434130c2bbe55e1c82ca4319aeebcb8840b49e40
Oleg Giberstein <oleg@coinrule.com>
2025-06-30 13:16:32 +0100

    fix: Exclude scraping packages from server bundle

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['playwright-extra', 'puppeteer-extra-plugin-stealth'],
+  },
 };
 
 export default nextConfig;
